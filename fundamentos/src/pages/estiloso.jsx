b2a1d897c1f487418fe79b693b9f70eaaeb8e2f5
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-12 11:36:35 +0200

    trabalhando com css modularizado

---
+++ fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import estilos from '../styles/Estiloso.module.css'
+
+export default function Estiloso() {
+    return (
+        <div className={estilos.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+}
