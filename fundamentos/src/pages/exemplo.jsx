e32d589533d0b8095ac1123b268c0fb95c110398
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-12 11:42:35 +0200

    exemplo de uso de componentes no next/react

---
+++ fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,10 @@
+import { Cabecalho } from "@/components/Cabecalho";
+
+export default function Exemplo() {
+    return (
+        <>
+            <Cabecalho titulo="Teste titulo" subtitulo="Teste subtitulo" />
+            <Cabecalho titulo="Teste 2" subtitulo="subtitulo" />
+        </>
+    )
+}
